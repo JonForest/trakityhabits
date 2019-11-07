953b8535ba0a96fdd461d5912fc20a600b196c62
Jonathan Hollingsworth <jonforest@gmail.com>
2019-11-08 07:00:36 +1300

    Added date regex match for the daily habits route, and available on the Habits component

--- src/app/routes/Habits/index.js
+++ src/app/routes/Habits/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Habits() {
+export default function Habits({ match: { params: { date }}}) {
 	const [habits, updateHabits] = useState([])
 
 
--- src/app/routes/Router.js
+++ src/app/routes/Router.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Habits from './Habits';
+import Habits from './Habits'
 import Dashboard from './Dashboard';
 
 export default function Router() {
   return (
     <Switch>
-      <Route path="/:date">
-        <Habits />
-      </Route>
+      <Route path="/:date(\d{4}-\d{1,2}-\d{1,2})" component={Habits} />
 
       <Route path="/">
         <Dashboard />
