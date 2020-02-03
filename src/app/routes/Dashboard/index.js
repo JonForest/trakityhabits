7172a0dfe3dddbfe0bdd0775da7a9a1517ba3ef4
Jonathan Hollingsworth <jonforest@gmail.com>
2020-02-04 07:05:31 +1300

    Add ability to select last number of days to fetch

--- src/app/routes/Dashboard/index.js
+++ src/app/routes/Dashboard/index.js
@@ -7,6 +7,7 @@ import ProgressChart from '../../components/ProgressChart';
 
 export default function Dashboard() {
   const [days, updateDays] = useState([]);
+  const [numbDays, updateNumbDays] = useState(20);
   const [currentStreak, updateCurrentStreak] = useState(undefined);
   const [longestStreak, updateLongestStreak] = useState(undefined);
 
@@ -16,6 +17,7 @@ export default function Dashboard() {
     return db
       .collection(`users/${uid}/days`)
       .orderBy('date', 'desc')
+      .limit(numbDays)
       .onSnapshot(async querySnapshot => {
         const days = [];
         querySnapshot.forEach(doc => {
@@ -34,13 +36,19 @@ export default function Dashboard() {
           updateLongestStreak(getLongestStreak(days));
         }
       });
-  }, [updateDays, updateCurrentStreak, uid]);
+  }, [updateDays, updateCurrentStreak, numbDays, uid]);
 
   return (
     <Layout title="Dashboard" linkTo={getFormattedDate(new Date())} linkText="See Today">
       <div className="flex">
         <div className="flex-grow" />
         <div className="flex-grow-0 w-11/12 md:w-4/6 xl:w-1/6">
+          Show last:
+          <ul className="mb-4">
+            <li className="inline pr-4">{numbDays === 10 ? 10 : <button className="text-blue-600 underline" onClick={() => updateNumbDays(10)}>10</button>}</li>
+            <li className="inline pr-4">{numbDays === 20 ? 20 : <button className="text-blue-600 underline" onClick={() => updateNumbDays(20)}>20</button>}</li>
+            <li className="inline">{numbDays === 30 ? 30 : <button className="text-blue-600 underline" onClick={() => updateNumbDays(30)}>30</button>}</li>
+          </ul>
           {currentStreak !== undefined && <Progress currentStreak={currentStreak} longestStreak={longestStreak} />}
         </div>
         <div className="flex-grow" />
