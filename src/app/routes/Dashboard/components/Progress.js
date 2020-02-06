29fb81803aa384ded316637f79a2b58636f0ec6a
Jonathan Hollingsworth <jonforest@gmail.com>
2020-02-07 10:11:15 +1300

    Small fix to stop the progress ball overrunning the text

--- src/app/routes/Dashboard/components/Progress.js
+++ src/app/routes/Dashboard/components/Progress.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProgressBall from '../../../components/ProgressBall';
 
+// todo: only render if the currentStreak or longestStreak are different to the last render
 export default function Progress({ currentStreak, longestStreak }) {
   // Need to calculate the relative lengths of each streak
   // Assume that 5 days will be 100%, unless the longest streak is longer
@@ -13,15 +14,15 @@ export default function Progress({ currentStreak, longestStreak }) {
   return (
     <div className="flex w-20 flex-col w-full text-left mb-8">
       <div className="flex w-full">
-        <div className="w-1/5 flex-shrink-0 pr-3">Longest streak</div>
-        <div className="w-4/5 flex-grow-0">
+        <div className="w-3/12 flex-shrink-0 pr-3">Longest streak</div>
+        <div className="w-9/12 flex-grow-0 pl-2">
           <ProgressBall value={longestStreak} percentage={longestStreakLength} animationDuration={3} />
         </div>
       </div>
 
       <div className="flex w-full content-start">
-        <div className="w-1/5 flex-shrink-0 pr-3">Current streak</div>
-        <div className="w-4/5 flex-grow-0 items-start">
+        <div className="w-3/12 flex-shrink-0 pr-3">Current streak</div>
+        <div className="w-9/12 flex-grow-0 pl-2">
           <ProgressBall value={currentStreak} percentage={currentStreakLength} animationDuration={currentStreakTime} />
         </div>
       </div>
